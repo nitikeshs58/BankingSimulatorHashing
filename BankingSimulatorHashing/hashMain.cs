5009fa2fa2effa32d614efe343e5b7b00a374556
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-06 23:45:38 +0530

    Added: Welcome message to banking simulator using hashing.

---
+++ BankingSimulatorHashing/hashMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingSimulatorHashing
+{
+    class hashMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to banking simulator using hashing!");
+        }
+    }
+}
